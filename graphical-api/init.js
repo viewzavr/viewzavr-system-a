83f743d242e4b1496cdd583c204cd988aed93941
Pavel Vasev <pavel.vasev@gmail.com>
2021-10-10 01:14:30 +0500

    better

--- graphical-api/init.js
+++ graphical-api/init.js
@@ -6,7 +6,7 @@ import vis from "./vis.js";
 
 import utils_setup from             "./utils/init.js";
 import vis_threejs_setup from       "./vis-threejs/init.js";
-import components_setup from        "./components/init.js";
+//import components_setup from        "./components/init.js";
 import components_threejs_setup from "./components-threejs/init.js";
 
 export function setup( vz, opts={} ) {
@@ -19,6 +19,6 @@ export function setup( vz, opts={} ) {
   vis( vz );
   vis_threejs_setup( vz );
 
-  components_setup( vz );
+//  components_setup( vz );
   components_threejs_setup( vz );
 }
